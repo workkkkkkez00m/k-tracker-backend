09180584e1c2c86496568a3ea9aa5059e9b2f775
LI,WEI-MING <workonly1022@gmail.com>
2025-08-29 14:05:08 +0800

    new headers

--- server.js
+++ server.js
@@ -73,7 +73,15 @@ function executeQuery(query, params = []) {
 // ===================================================
 async function fetchAndParseSales(url) {
     try {
-        const headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36' };
+        const headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+            'Accept-Language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
+            'Cache-Control': 'max-age=0'
+        };
         const response = await axios.get(url, { headers });
         const $ = cheerio.load(response.data);
 
